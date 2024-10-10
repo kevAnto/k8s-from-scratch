c9280393405dff0ae7551b3aa67868d8d5e9afde
Antoine CHOULA <76167680+kevAnto@users.noreply.github.com>
2024-10-11 00:26:46 +0200

    Script installation

--- k8s-install.sh
+++ k8s-install.sh
@@ -1,19 +1,30 @@
-echo "Make script executable using chmod u+x FILE_NAME.sh"
+#!/bin/bash
 
-sudo apt-get update
+#Passing enterings nodes names to ip maping on every nodes
+sudo swapoff -a
+# sudo vim /etc/hosts
+# 10.0.10.247 controlplane
+# 10.0.20.95 workerNode
 
-# apt-transport-https may be a dummy package; if so, you can skip that package
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+#Place your on controlplan and set for controlplan also do same for workers
+# sudo hostnamectl set-hostname controlplane
+# sudo hostnamectl set-hostname workerNode
 
+#Install a container runtime
+# Enable IPv4 packet forwarding https://kubernetes.io/docs/setup/production-environment/container-runtimes/#prerequisite-ipv4-forwarding-optional
+# Install Containerd https://github.com/containerd/containerd/blob/main/docs/getting-started.md#option-2-from-apt-get-or-dnf
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+wget https://github.com/kevAnto/k8s-from-scratch/blob/main/containerd-install.sh
+chmod u+x ./containerd-install.sh
+service containerd status
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#Install kubeadm, kubelet and kubectl
 
-sudo apt-get update
+wget https://github.com/kevAnto/k8s-from-scratch/blob/main/k8s-install.sh
+chmod u+x ./k8s-install.sh
+kubeadm version
+service kubelet status
 
-echo "Installing latest versions"
-sudo apt-get install -y kubelet kubeadm kubectl
-
-echo "Fixate version to prevent upgrades"
-sudo apt-mark hold kubelet kubeadm kubectl
+#kubectl --version
+#kubeadm version
+#kubectl version
